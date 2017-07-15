bd4d5e6319b9dc3e73ce69cf2f584beadbdee14e
Elizabeth Niebauer <egniebauer@gmail.com>
2017-07-15 13:34:52 -0500

    Initial commit

---
+++ src/main/java/com/egniebauer/KidFriendlyStlApplication.java
@@ -0,0 +1,12 @@
+package com.egniebauer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidFriendlyStlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidFriendlyStlApplication.class, args);
+	}
+}
