138d7dff6ed1683c114671e92c08f1b4f9b0e099
Elizabeth Niebauer <egniebauer@gmail.com>
2017-07-21 22:02:18 -0500

    update business form to prepopulate for edit

--- src/main/java/com/egniebauer/models/forms/BusinessForm.java
+++ src/main/java/com/egniebauer/models/forms/BusinessForm.java
@@ -1,7 +1,13 @@
 package com.egniebauer.models.forms;
 
+import com.egniebauer.models.AgeRange;
+import com.egniebauer.models.BestTime;
+import com.egniebauer.models.Business;
+import com.egniebauer.models.Category;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,12 +49,32 @@ public class BusinessForm {
         this.categoryIds = categoryIds;
     }
 
+    public void addCategoryIds(List<Category> categories) {
+        List<Integer> ids = new ArrayList<>();
+
+        for (Category cat : categories) {
+            ids.add(cat.getId());
+        }
+
+        setCategoryIds(ids);
+    }
+
     public List<Integer> getAgeRangeIds() {
         return ageRangeIds;
     }
 
     public void setAgeRangeIds(List<Integer> ageRangeIds) {
         this.ageRangeIds = ageRangeIds;
+    }
+
+    public void addAgeRangeIds(List<AgeRange> ageRanges) {
+        List<Integer> ids = new ArrayList<>();
+
+        for (AgeRange range : ageRanges) {
+            ids.add(range.getId());
+        }
+
+        setAgeRangeIds(ids);
     }
 
     public List<Integer> getBestTimesIds() {
@@ -59,11 +85,29 @@ public class BusinessForm {
         this.bestTimesIds = bestTimesIds;
     }
 
+    public void addBestTimesIds(List<BestTime> bestTimes) {
+        List<Integer> ids = new ArrayList<>();
+
+        for (BestTime time : bestTimes) {
+            ids.add(time.getId());
+        }
+
+        setBestTimesIds(ids);
+    }
+
     public Integer getBusinessId() {
         return businessId;
     }
 
     public void setBusinessId(Integer businessId) {
         this.businessId = businessId;
+    }
+
+    public void prepopulateForm(Business editBusiness) {
+        setBusinessName(editBusiness.getName());
+        addCategoryIds(editBusiness.getCategories());
+        addAgeRangeIds(editBusiness.getAgeRanges());
+        addBestTimesIds(editBusiness.getBestTimes());
+        setBusinessId(editBusiness.getId());
     }
 }
