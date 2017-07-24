c5df25e1655543238dcaf92c09f3bf19192abf59
Elizabeth Niebauer <egniebauer@gmail.com>
2017-07-24 15:37:33 -0500

    Add view handler to controller

--- src/main/java/com/egniebauer/controllers/BestTimeController.java
+++ src/main/java/com/egniebauer/controllers/BestTimeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,12 +18,27 @@ import javax.validation.Valid;
  */
 
 @Controller
-@RequestMapping(value = "admin/times")
+@RequestMapping(value = "admin/time")
 public class BestTimeController {
 
     @Autowired
     private BestTimeDao bestTimeDao;
 
+    @RequestMapping(value = "view/{id}", method = RequestMethod.GET)
+    public String displayInformation(Model model, @PathVariable int id) {
+
+        try {
+
+            BestTime bestTime = bestTimeDao.findOne(id);
+            model.addAttribute("h1", bestTime.getName());
+            model.addAttribute("bestTime", bestTime);
+            return "bestTime/view";
+
+        } catch (IllegalArgumentException e) {
+
+            return "redirect:/admin";
+        }
+    }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddTime(Model model) {
