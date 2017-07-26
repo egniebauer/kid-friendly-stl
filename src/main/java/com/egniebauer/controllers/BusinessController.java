1fc3bfd132e53fde185e7d202dbe855489f1d865
Elizabeth Niebauer <egniebauer@gmail.com>
2017-07-25 22:56:58 -0500

    Add processAddEdit handler

--- src/main/java/com/egniebauer/controllers/BusinessController.java
+++ src/main/java/com/egniebauer/controllers/BusinessController.java
@@ -43,7 +43,6 @@ public class BusinessController {
 
         model.addAttribute("h1", "Business Listings");
         model.addAttribute("businesses", businessDao.findAll());
-
         return "business/index";
     }
 
@@ -64,67 +63,70 @@ public class BusinessController {
     }
 
     @RequestMapping(value = "admin/business/add", method = RequestMethod.GET)
-    public String displayAddBusiness(Model model) {
+    public String displayAdd(Model model) {
 
         model.addAttribute("h1", "Add Business");
-        model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("ageRanges", ageRangeDao.findAll());
-        model.addAttribute("bestTimes", bestTimeDao.findAll());
-
+        model.addAttribute("categories", categoryDao.findAll() );
+        model.addAttribute("ageRanges", ageRangeDao.findAll() );
+        model.addAttribute("bestTimes", bestTimeDao.findAll() );
         model.addAttribute("form", new BusinessForm());
         model.addAttribute("submitText", "Add Business");
-
         return "business/add-edit";
-    }
-
-    @RequestMapping(value = "admin/business/add", method = RequestMethod.POST)
-    public String processAddBusiness(@ModelAttribute @Valid BusinessForm form,
-                                     Errors errors, Model model) {
-
-        if (errors.hasErrors()) {
-            model.addAttribute("h1", "Add Business");
-            model.addAttribute("categories", categoryDao.findAll());
-            model.addAttribute("ageRanges", ageRangeDao.findAll());
-            model.addAttribute("bestTimes", bestTimeDao.findAll());
-
-            model.addAttribute("form", new BusinessForm());
-            model.addAttribute("submitText", "Add Business");
-
-            return "business/add-edit";
-        }
-
-        Business newBusiness = new Business( form.getBusinessName() );
-
-        addDetailsFromIds(newBusiness, "category", form.getCategoryIds() );
-        addDetailsFromIds(newBusiness, "ageRange", form.getAgeRangeIds() );
-        addDetailsFromIds(newBusiness, "bestTimes", form.getBestTimesIds() );
-
-        businessDao.save(newBusiness);
-        return "redirect:/admin";
     }
 
     @RequestMapping(value = "admin/business/edit/{id}", method = RequestMethod.GET)
-    public String displayEditBusiness(Model model, @PathVariable int id) {
+    public String displayEdit(Model model, @PathVariable int id) {
 
-        BusinessForm editForm = new BusinessForm();
+        BusinessForm form = new BusinessForm();
         Business editBusiness = businessDao.findOne(id);
-        editForm.prepopulateForm(editBusiness);
-
+        form.prepopulateForm(editBusiness);
+        model.addAttribute("categories", categoryDao.findAll() );
+        model.addAttribute("ageRanges", ageRangeDao.findAll() );
+        model.addAttribute("bestTimes", bestTimeDao.findAll() );
         String h1 = "Edit " + editBusiness.getName();
         model.addAttribute("h1", h1);
-
-        model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("ageRanges", ageRangeDao.findAll());
-        model.addAttribute("bestTimes", bestTimeDao.findAll());
-
-        model.addAttribute("form", editForm);
+        model.addAttribute("form", form);
         model.addAttribute("submitText", "Update");
-
         return "business/add-edit";
     }
 
 
+    @RequestMapping(value = "admin/business/add-edit", method = RequestMethod.POST)
+    public String processAddEdit(@ModelAttribute @Valid BusinessForm form,
+                                     Errors errors, Model model) {
 
+        if (errors.hasErrors()) {
+            if (businessDao.exists(form.getBusinessId())) {
+
+                String h1 = "Edit " + form.getBusinessName();
+                model.addAttribute("h1", h1);
+                model.addAttribute("categories", categoryDao.findAll() );
+                model.addAttribute("ageRanges", ageRangeDao.findAll() );
+                model.addAttribute("bestTimes", bestTimeDao.findAll() );
+                model.addAttribute("submitText", "Update");
+                return "business/add-edit";
+            }
+
+            model.addAttribute("h1", "Add Business");
+            model.addAttribute("form", new BusinessForm());
+            model.addAttribute("categories", categoryDao.findAll() );
+            model.addAttribute("ageRanges", ageRangeDao.findAll() );
+            model.addAttribute("bestTimes", bestTimeDao.findAll() );
+            model.addAttribute("submitText", "Add Business");
+            return "business/add-edit";
+        }
+
+        Business business = new Business( form.getBusinessName() );
+        addDetailsFromIds(business, "category", form.getCategoryIds() );
+        addDetailsFromIds(business, "ageRange", form.getAgeRangeIds() );
+        addDetailsFromIds(business, "bestTimes", form.getBestTimesIds() );
+
+        if (form.getBusinessId() != null) {
+            business.setId(form.getBusinessId());
+        }
+        businessDao.save(business);
+        return "redirect:/admin";
+    }
 
     private void addDetailsFromIds(Business newBusiness, String type, Iterable<Integer> ids) {
 
